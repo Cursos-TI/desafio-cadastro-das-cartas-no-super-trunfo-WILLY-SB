d9cceaa8d0a7689f17556d963a5421a949c48f48
WILLY-SB <willysoaresdebrito@gmail.com>
2025-04-26 23:57:52 +0000

    novato01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
 int main(){
- 
-   int População = 1500,;
-   float Área=100.000;
-   float PIB =100.000;
+ char Estado: Ceara;
+ char Codigo da carta: A01,AO2:
+ char Nome da cidade: Fortaleza;
+ int População: 100000;
+ float Area: 314,9;
+ float PIB: 3,14;
+ int Numero de pontos turístcos: 10;
 
-   printf("População:%d\n", Popilação);
-   escanf("%d", &População);
+ printf("Estado: %s\n", Estado);
+ escanf("%s", Estado);
 
-   printf("Área:%f\n", Área);
-   escanf("%f", &Área);
+ printf("Codigo da carta: %s\n", Codigo da carta);
+escanf("%S", Codigo da carta =A01);
 
-   printf("PIB:%F\n", PIB);
-   eacanf("%f", &PIB);
+printf("Nome da cidade: %s\n", Nome da cidade);
+escanf("%s", Nome da cidade);
 
-}
+printf("populaçao: %d\n", população);
+escanf("%d", populaçao);
 
-   printf("População:%d\n", População);
-   printf("Área:%f\n", Área);
-   printf("PIB:%f\n", PIB);
+printf("Area: %f\n', Area);
+   escanf("%f", Area);
+   
+   printf("PIB: %f\n", PIB);
+   Escanf("%f", PIB);
+   
+   printf("Numero de pontos turiscos: %d\n", Numero de pontos turiscos);
+   escanf("%d", Numero de pontos turisticos);
 
+   printf("Estado: %s\n", Estado);
+   escanf("%s", Estado);
+
+   printf("Codigo da carta: %s\n", Codigo da carta);
+   escanf("%s", Codigo da carta B02);
+
+   printf("Nome da cidade: %s\n", Nome da cidade);
+   escanf("%s", Nome da cidade);
+
+   printf("populaçao: %d\n", populaçao);
+   escanf("%d", populaçao);
+
+   printf("Area: %f\n", Area);
+   escanf("%f", Area);
+
+   printf("PIB: %f\n", PIB);
+   escanf("%f", PIB);
 
    return 0;
 }
