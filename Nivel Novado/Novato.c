2e46f2d561f28dc973d925fb8673536ad33e7f23
WILLY-SB <willysoaresdebrito@gmail.com>
2025-04-28 21:46:54 +0000

    novato01

---
+++ Nivel Novado/Novato.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+main() {
+    int Populacão;
+    float Area;
+    float PIB;
+    int Numero de Pontos turistico;
+
+    printf("Digite a população);
+        scanf("%d", &População);
+
+        printf("Digite a área);
+        escanf("%f", &Area);
+
+        printf("Digite o PIB);
+            scanf("%f", &PIB);
+            
+        printf("Digite o número de pontos turisticos);
+        scanf("%d", &Numero de pontos turistico);
+
+        return 0;
